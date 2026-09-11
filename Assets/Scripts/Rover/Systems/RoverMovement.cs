6b284d91b8b71d47e829f41a615be6b501749383
Kokos-s <aninibas@gmail.com>
2026-09-11 01:52:57 -0300

    Tune rover driving dynamics
    - Add braking before switching driving direction.
    - Adjust mass, motor torque, top speed, steering angle, and wheel settings.
    
    Other changes:
    - Smooth terrain in several areas to prevent the rover from getting stuck.

--- Assets/Scripts/Rover/Systems/RoverMovement.cs
+++ Assets/Scripts/Rover/Systems/RoverMovement.cs
@@ -13,11 +13,14 @@ public class RoverMovement : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private RoverEnergy energy;
 
-    [SerializeField] private float motorForce = 2000f;
-    [SerializeField] private float maxSpeed = 20f;
-    [SerializeField] private float steerSmoothSpeed = 0.3f;
+    [SerializeField] private float motorForce = 5000f;
+    [SerializeField] private float maxSpeed = 17f;
+    [SerializeField] private float brakeForce = 3000f;
+    [SerializeField] private float directionChangeSpeed = 0.2f;
+
+    [SerializeField] private float steerSmoothSpeed = 1.5f;
     private float currentSteerAngle = 0f;
-    [SerializeField] private float maxSteerAngle = 25f;
+    [SerializeField] private float maxSteerAngle = 21f;
 
     [SerializeField] private Transform meshFrontLeft;
     [SerializeField] private Transform meshMiddleLeft;
@@ -25,8 +28,6 @@ public class RoverMovement : MonoBehaviour
     [SerializeField] private Transform meshFrontRight;
     [SerializeField] private Transform meshMiddleRight;
     [SerializeField] private Transform meshRearRight;
-
-    private Vector3 meshRotationOffset = new Vector3(0f, 0f, 0f);
 
     private void Awake()
     {
@@ -46,30 +47,46 @@ public class RoverMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //Debug.Log(GetComponent<Rigidbody>().inertiaTensor);
         Vector2 moveInput = controls.Rover.Move.ReadValue<Vector2>();
-        float currentSpeed = rb.linearVelocity.magnitude;
-        float motorPowerReductionFactor = Mathf.Max(0f, 1f - (currentSpeed / maxSpeed));
-        float motorTorque = 0f;
-        if (energy.CurrentEnergy > 0f)
-            motorTorque = moveInput.y * motorForce * motorPowerReductionFactor;
 
-        ApplyMotorTorque(wheelFrontLeft, motorTorque);
-        ApplyMotorTorque(wheelMiddleLeft, motorTorque);
-        ApplyMotorTorque(wheelRearLeft, motorTorque);
-        ApplyMotorTorque(wheelFrontRight, motorTorque);
-        ApplyMotorTorque(wheelMiddleRight, motorTorque);
-        ApplyMotorTorque(wheelRearRight, motorTorque);
+        float throttle = 0f;
+
+        if (Mathf.Abs(moveInput.y) > 0.1f)
+            throttle = moveInput.y;
+
+        float forwardSpeed = Vector3.Dot(rb.linearVelocity, transform.forward);
+        float currentSpeed = rb.linearVelocity.magnitude;
+        bool isBraking = false;
+
+        if ((forwardSpeed > directionChangeSpeed && throttle < 0f)  ||  (forwardSpeed < -directionChangeSpeed && throttle > 0f))
+            isBraking = true;
+        float motorTorque = 0f;
+        float brakeTorque = 0f;
+
+        if (isBraking)
+            brakeTorque = Mathf.Abs(throttle) * brakeForce;
+        else if (throttle != 0f && energy.CurrentEnergy > 0f)
+        {
+            float motorPowerReductionFactor = 1f - Mathf.InverseLerp(maxSpeed * 0.6f, maxSpeed, currentSpeed);
+            motorTorque = throttle * motorForce * motorPowerReductionFactor;
+        }
+
+        ApplyWheelForces(wheelFrontLeft, motorTorque, brakeTorque);
+        ApplyWheelForces(wheelMiddleLeft, motorTorque, brakeTorque);
+        ApplyWheelForces(wheelRearLeft, motorTorque, brakeTorque);
+        ApplyWheelForces(wheelFrontRight, motorTorque, brakeTorque);
+        ApplyWheelForces(wheelMiddleRight, motorTorque, brakeTorque);
+        ApplyWheelForces(wheelRearRight, motorTorque, brakeTorque);
 
         float targetSteerAngle = moveInput.x * maxSteerAngle;
+
         currentSteerAngle = Mathf.MoveTowards(currentSteerAngle, targetSteerAngle, steerSmoothSpeed);
+
         ApplySteerAngle(wheelFrontLeft, currentSteerAngle);
         ApplySteerAngle(wheelFrontRight, currentSteerAngle);
- 
+
         if (Mathf.Abs(moveInput.y) > 0.1f)
-        {
             energy.ReportPlayerMovement();
-        }
     }
 
     void Update()
@@ -86,29 +103,25 @@ public class RoverMovement : MonoBehaviour
     {
         Vector3 position;
         Quaternion rotation;
+
         collider.GetWorldPose(out position, out rotation);
 
         mesh.position = position;
-        mesh.rotation = rotation * Quaternion.Euler(meshRotationOffset);
+        mesh.rotation = rotation;
     }
 
-        void ApplyMotorTorque(WheelCollider wheel, float torque)
+    void ApplyWheelForces(WheelCollider wheel, float motorTorque, float brakeTorque)
     {
         if (wheel.isGrounded)
-        {
-            wheel.motorTorque = torque;
-        }
+            wheel.motorTorque = motorTorque;
         else
-        {
             wheel.motorTorque = 0f;
-        }
+
+        wheel.brakeTorque = brakeTorque;
     }
-        void ApplySteerAngle(WheelCollider wheel, float angle)
+
+    void ApplySteerAngle(WheelCollider wheel, float angle)
     {
-        if (wheel.isGrounded)
-        {
-            wheel.steerAngle = angle;
-        }
+        wheel.steerAngle = angle;
     }
 }
-   
